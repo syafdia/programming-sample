5fbecc876df56d0d8e31fea58c72c525dcd24001
Syafdia Okta <syafdia@gmail.com>
2019-12-27 06:22:05 +0700

    add To Do List laravel

---
+++ LaravelToDo/database/migrations/2019_12_26_223539_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('status');
+            $table->timestamps();
+
+            $table->index(['status']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
